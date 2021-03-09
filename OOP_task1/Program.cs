48ae54dafe4ca3753fdb6f09150811eac5ecbfe9
Evgeny.Popov <evpop@telenor.dk>
2021-03-09 19:12:22 +0200

    Task 2 is added

--- OOP_task1/Program.cs
+++ OOP_task1/Program.cs
@@ -4,15 +4,71 @@ namespace HomeWork_1
 {
     class Program
     {
+        // Задание №2 - Класс для расчета влезет ли квадрат в круг или нет
+        class Square
+        {
+            private double side; //Сторона квадрата
+            private double diametr; //Диаметр окружности
+
+            public void GetShape()
+            {
+                Console.WriteLine("\nДавайте теперь определим поместится ли квадрат в круг и наоборот :) ");
+                const double P = 3.14;
+                Console.Write("\nВведите площадь круга: ");
+                double S1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.Write("Введите площадь квадрата: ");
+                double S2 = Convert.ToDouble(Console.ReadLine());
+
+                double Diametr = Math.Sqrt(S1 / P) * 2;
+                double Side = Math.Sqrt(S2);
+
+                if (Diametr <= Side)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("a) Уместится");
+                }
+
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("a) Не уместится");
+                }
+
+                double Diagonal = Side * Math.Sqrt(2);
+
+                if (Diagonal <= Diametr)
+                    Console.WriteLine("б) Уместится");
+
+                else
+                    Console.WriteLine("б) Не уместится");
+            }
+            double Side
+            {
+                get { return side; }
+                set { side = value; }
+            }
+
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Программа считает площадь квадрата и круга");
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Программа считает площадь квадрата и круга");
+            
+            
 
             for (int i = 0; i <= 2; i++)
             {
+                
                 Console.WriteLine("\nПопытка №{0}", i + 1);
                 Console.Write("Введите сторону квадрата: ");
+                
                 var sideA = double.Parse((Console.ReadLine()));
                 if (sideA <= 0)
                 {
@@ -72,43 +128,16 @@ namespace HomeWork_1
                 Random random = new Random();
                 return random.NextDouble() * (maximum - minimum) + minimum;
             }
-            Console.ReadLine();
 
-            const double P = 3.14;
+            // Создаем экземпляр класса и вызываем конструктор для проверки размеров фигур
+            Square fitToSircle = new Square();
 
-            Console.Write("Введите площадь круга: ");
-            double S1 = Convert.ToDouble(Console.ReadLine());
+            fitToSircle.GetShape();
 
-            Console.WriteLine();
-            Console.Write("Введите площадь квадрата: ");
-            double S2 = Convert.ToDouble(Console.ReadLine());
-
-            double Diametr = Math.Sqrt(S1 / P) * 2;
-            double Side = Math.Sqrt(S2);
-
-            if (Diametr <= Side)
-            {
-                Console.WriteLine();
-                Console.WriteLine("a) Уместится");
-            }
-
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("a) Не уместится");
-            }
-
-            double Diagonal = Side * Math.Sqrt(2);
-
-            if (Diagonal <= Diametr)
-                Console.WriteLine("б) Уместится");
-
-            else
-                Console.WriteLine("б) Не уместится");
 
             Console.ReadKey();
         }
-
+       
     }
 }
 
---
+++ OOP_task1/Square.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_task1
+{
+    // ** Копия класса, сюда смотреть не надо, он есть в файле Program.cs
+    // Задание №2 - Класс для расчета влезет ли квадрат в круг или нет)
+    class Square
+    {
+        private double side; //Сторона квадрата
+        private double diametr; //Диаметр окружности
+
+        public void GetShape()
+        {
+            Console.WriteLine("\nДавайте теперь определим поместится ли квадрат в круг и наоборот :) ");
+            const double P = 3.14;
+            Console.Write("\nВведите площадь круга: ");
+            double S1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Введите площадь квадрата: ");
+            double S2 = Convert.ToDouble(Console.ReadLine());
+
+            double Diametr = Math.Sqrt(S1 / P) * 2;
+            double Side = Math.Sqrt(S2);
+
+            if (Diametr <= Side)
+            {
+                Console.WriteLine();
+                Console.WriteLine("a) Уместится");
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("a) Не уместится");
+            }
+
+            double Diagonal = Side * Math.Sqrt(2);
+
+            if (Diagonal <= Diametr)
+                Console.WriteLine("б) Уместится");
+
+            else
+                Console.WriteLine("б) Не уместится");
+        }
+        double Side
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+
+    }
+}
